56dcb1d6ed2a5fdf9a3f28bbac7c378a472a6aba
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-22 01:25:32 -0300

    chore: install theme switch

---
+++ components/theme-switch.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {useState, useEffect} from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export function ThemeSwitch() {
+  const { theme, setTheme } = useTheme()
+
+  //essa variavel serve para controlar quando o componente foi montado no cliente. 
+  //Isso evita que o componente seja renderizado no servidor, onde o tema ainda não está disponível.
+  const [mounted, setMounted] = useState(false)
+  
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+
+  return (
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+      {theme === "light" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
