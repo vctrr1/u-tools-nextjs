e3c106db34775e2b4df4c2f7d1b32db2b5a722cb
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-27 18:35:41 -0300

    create currency page

--- app/currency/page.tsx
+++ app/currency/page.tsx
@@ -1,8 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ArrowDownUp } from 'lucide-react';
+
 function Currency() {
     return ( 
-        <div>
-            <h1>helloCurrency</h1>
-        </div>
+        <main className="flex w-full h-[100%] items-center justify-center">
+            <Card className="w-[90%]">
+                <CardHeader className="text-center text-lg">
+                    Faça sua conversão
+                </CardHeader>
+                <CardContent>
+                    <div className="flex flex-col items-center space-y-2 pb-5">
+                        <Input/>
+                        <ArrowDownUp size={18} strokeWidth={1.25}/>
+                        <Input/>
+                    </div>
+                    <Button className="w-full">Converter</Button>
+                </CardContent>
+            </Card>
+        </main>
      );
 }
 
