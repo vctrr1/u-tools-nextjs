65b7b37d3b6c9c95ff686dd7245fbaddbef993b6
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-28 10:02:30 -0300

    fix: Currency page to small devices and add animation to result

--- app/currency/page.tsx
+++ app/currency/page.tsx
@@ -2,11 +2,12 @@
 
 import { SelectCurrency } from "@/components/select-currency";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ArrowLeftRight  } from 'lucide-react';
 import { useState } from "react";
 import CurrencyConversion from "@/actions/currency-api";
+import {motion} from "framer-motion"
 
 function Currency() {
     
@@ -29,37 +30,46 @@ function Currency() {
     }
 
     return ( 
-        <main className="flex w-full h-[100%] items-center justify-center">
-            <Card className="w-[90%]">
+        <main className="flex flex-col w-full h-[100%] items-center justify-center">
+            <Card className="w-[90%] sm:w-[90%] md:w-[90%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] mt-5 mb-5">
                 <CardHeader className="text-center text-lg">
-                    Faça sua conversão
+                    <CardTitle className="text-center text-2xl">
+                        Faça sua conversão
+                    </CardTitle>
                 </CardHeader>
                 <CardContent>
                     <div className="flex flex-col items-center space-y-3 pb-5">
                         <div className="flex flex-col w-full">
-                            <label>Digite a quantidade:</label>
+                            <label className="text-lg">Digite a quantidade:</label>
                             <Input type="number" onChange={(e) => setAmount(Number(e.target.value))}/>
                         </div>
                         <div className="flex space-x-4 items-center w-full">
                             <div className="w-full">
-                                <label>Moeda Origem:</label>
+                                <label className="text-lg">Moeda Origem:</label>
                                 <SelectCurrency onChange={setCurrencyBase} value={currencyBase}/>
                             </div>
                             <ArrowLeftRight size={18} strokeWidth={1.25} className="flex-shrink-0 mt-5"/>
                             <div className="w-full">
-                                <label>Moeda Destino:</label>
+                                <label className="text-lg">Moeda Destino:</label>
                                 <SelectCurrency onChange={setCurrencyTarget} value={currencyTarget}/>
                             </div>
                         </div>
                     </div>
-                    <Button onClick={() => handleSubmit(currencyBase, currencyTarget)} className="w-full">Converter</Button>
-                    {amount !== 0 && rates !== null &&(
-                        <div className="text-center mt-4 text-lg">
-                          Resultado: {amount} {currencyBase} = {(amount * rates).toFixed(2)} {currencyTarget}
-                        </div>
-                    )}
+                    <Button onClick={() => handleSubmit(currencyBase, currencyTarget)} className="w-full text-base">Converter</Button>
                 </CardContent>
             </Card>
+            {amount !== 0 && rates !== null && (
+                <motion.div 
+                    initial={{ opacity: 0, y: 20 }} // Início da animação: invisível e um pouco abaixo
+                    animate={{ opacity: 1, y: 0 }} // Animação final: visível e na posição original
+                    transition={{ duration: 0.9, ease: "easeOut" }} // Duração e tipo da animação
+                    className="w-[90%] sm:w-[90%] md:w-[90%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]"
+                >
+                    <div className="border shadow-sm p-4 rounded-lg text-center font-semibold text-lg">
+                        Resultado: {amount} {currencyBase} = {(amount * rates).toFixed(2)} {currencyTarget}
+                    </div>
+                </motion.div>
+            )}
         </main>
      );
 }
