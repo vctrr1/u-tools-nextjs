ddf8b6cbd533dfa8fcfae92d96d53250d2072706
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-27 21:47:13 -0300

    feat: created states to variables currency and take response of api

--- app/currency/page.tsx
+++ app/currency/page.tsx
@@ -1,10 +1,38 @@
+"use client"
+
 import { SelectCurrency } from "@/components/select-currency";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { ArrowDownUp } from 'lucide-react';
+import { ArrowLeftRight  } from 'lucide-react';
+import { useState } from "react";
+import  axios from 'axios'
 
 function Currency() {
+    const [amount, setAmount] = useState(1)
+    const [currencyBase, setCurrencyBase] = useState("")
+    const [currencyTarget, setCurrencyTarget] = useState("")
+    const [rates, setRate] = useState<number| null>(null);
+
+    const handleSubmit = async () => {
+        if (!currencyBase || !currencyTarget) {
+            alert("Selecione as moedas.");
+            return;
+        }
+        
+        try {
+            const response = await axios.get(`https://v6.exchangerate-api.com/v6/412d8df4c3c863136783440a/pair/${currencyBase}/${currencyTarget}`)
+            setRate(response.data.conversion_rate)
+            console.log("API response:", rates);
+        } catch (error) {
+            console.error("Erro ao buscar câmbio:", error);
+            setRate(null); // Em caso de erro, reseta a taxa para `null`
+        }
+
+
+
+    }
+
     return ( 
         <main className="flex w-full h-[100%] items-center justify-center">
             <Card className="w-[90%]">
@@ -13,17 +41,23 @@ function Currency() {
                 </CardHeader>
                 <CardContent>
                     <div className="flex flex-col items-center space-y-3 pb-5">
-                        <div className="flex space-x-2 w-full">
-                            <SelectCurrency/>
-                            <Input/>
+                        <div className="flex flex-col w-full">
+                            <label>Digite a quantidade</label>
+                            <Input type="number"/>
                         </div>
-                        <ArrowDownUp size={18} strokeWidth={1.25}/>
-                        <div className="flex space-x-2 w-full">
-                            <SelectCurrency/>
-                            <Input/>
+                        <div className="flex space-x-4 items-center w-full">
+                            <div className="w-full">
+                                <label>Moeda Origem</label>
+                                <SelectCurrency/>
+                            </div>
+                            <ArrowLeftRight size={18} strokeWidth={1.25} className="flex-shrink-0 mt-5"/>
+                            <div className="w-full">
+                                <label>Moeda Destino</label>
+                                <SelectCurrency/>
+                            </div>
                         </div>
                     </div>
-                    <Button className="w-full">Converter</Button>
+                    <Button onClick={handleSubmit} className="w-full">Converter</Button>
                 </CardContent>
             </Card>
         </main>
