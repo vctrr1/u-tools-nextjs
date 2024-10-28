f745f8945b530c8bdbc605a27a9dd03b6bc9db08
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-28 18:18:22 -0300

    fix: calculator to small devices

--- app/calculadora/page.tsx
+++ app/calculadora/page.tsx
@@ -81,9 +81,9 @@ export default function Imc() {
 
     return ( 
         <main className=" flex w-full h-[100%] justify-center items-center">
-            <Card className="w-[85%] sm:w-[70%] md:w-[70%] lg:w-[50%] xl:w-[40%] 2xl:w-[30%] mt-5 mb-5">
-                <CardContent className="space-y-5">
-                    <input readOnly type="text" value={display} className={`${inputTheme}  h-16 w-full text-4xl text-right border-none mt-5 p-3 focus:outline-none`}/>
+            <Card className="w-[85%] sm:w-[60%] md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[30%] mt-5 mb-5">
+                <CardContent className="space-y-5 p-2">
+                    <input readOnly type="text" value={display} className={`${inputTheme} h-16 w-full text-4xl text-right border-none mt-5 p-3 focus:outline-none`}/>
                     <div className="grid grid-cols-4 gap-2 p-2 space-y-2 justify-items-center">
                         <button className="flex items-center justify-center h-14 w-14 rounded-full bg-orange-400 font-bold cursor-pointer mt-2" onClick={() => handleOperator('+')}><Plus size={19}/></button>
                         <button className="h-14 w-14 rounded-full bg-slate-400 font-bold cursor-pointer text-xl" onClick={() => appendToDisplay('7')}>7</button>
