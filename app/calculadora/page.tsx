78de2d73c67f5651379abc7580850725e76d00bc
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-28 15:17:20 -0300

    fix: color display of calculator to small devices

--- app/calculadora/page.tsx
+++ app/calculadora/page.tsx
@@ -1,14 +1,24 @@
 "use client"
 
 import { Card, CardContent } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Plus, Minus, Slash, Asterisk} from 'lucide-react';
+import { useTheme } from "next-themes";
+import { formatNumber } from "@/lib/format-number";
 
 export default function Imc() {
+
+    const {theme} = useTheme()
 
     const [display, setDisplay] = useState("")
     const [previousValue, setPreviousValue] = useState("")
     const [operator, setOperatore] = useState("")
+    const [inputTheme, setInputTheme] = useState("");
+    
+    useEffect(() => {
+        // Define o tema do input após o componente montar no cliente
+        setInputTheme(theme === "dark" ? "bg-[#030712]" : "bg-[#ffffff]");
+    }, [theme]);
 
     const appendToDisplay = (arg: string) => {
         setDisplay((prev) => prev + arg)
@@ -44,7 +54,7 @@ export default function Imc() {
                 default:
                     return
             }
-            setDisplay(result.toString())
+            setDisplay(formatNumber(result))
             setPreviousValue("")
             setOperatore("")
         }
@@ -72,10 +82,10 @@ export default function Imc() {
     return ( 
         <main className=" flex w-full h-[100%] justify-center items-center">
             <Card className="w-[85%] sm:w-[70%] md:w-[70%] lg:w-[50%] xl:w-[40%] 2xl:w-[30%] mt-5 mb-5">
-                <CardContent className="space-y-10">
-                    <input readOnly type="text" value={display} className="h-16 w-full text-4xl text-right border-none mt-5 p-3 focus:outline-none"/>
+                <CardContent className="space-y-5">
+                    <input readOnly type="text" value={display} className={`${inputTheme}  h-16 w-full text-4xl text-right border-none mt-5 p-3 focus:outline-none`}/>
                     <div className="grid grid-cols-4 gap-2 p-2 space-y-2 justify-items-center">
-                        <button className="flex items-center justify-center h-14 w-14 rounded-full bg-orange-400 font-bold cursor-pointer" onClick={() => handleOperator('+')}><Plus size={19}/></button>
+                        <button className="flex items-center justify-center h-14 w-14 rounded-full bg-orange-400 font-bold cursor-pointer mt-2" onClick={() => handleOperator('+')}><Plus size={19}/></button>
                         <button className="h-14 w-14 rounded-full bg-slate-400 font-bold cursor-pointer text-xl" onClick={() => appendToDisplay('7')}>7</button>
                         <button className="h-14 w-14 rounded-full bg-slate-400 font-bold cursor-pointer text-xl" onClick={() => appendToDisplay('8')}>8</button>
                         <button className="h-14 w-14 rounded-full bg-slate-400 font-bold cursor-pointer text-xl" onClick={() => appendToDisplay('9')}>9</button>
