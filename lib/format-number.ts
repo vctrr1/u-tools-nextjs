31811f9b14e332bb96f0f7a4bcefe94781093558
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-28 23:41:39 -0300

    fix: dont format number if number is interger

--- lib/format-number.ts
+++ lib/format-number.ts
@@ -1,4 +1,7 @@
 export function formatNumber (num: number, digits = 2){
+    if(Number.isInteger(num)){
+        return num.toString()
+    }
     return num.toLocaleString('pt-BR', {
         minimumFractionDigits: digits,
         maximumFractionDigits: digits
