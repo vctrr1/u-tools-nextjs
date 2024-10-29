802342b52f4eb6eea4c57272c4fc38e703820a57
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-29 01:09:16 -0300

    feat: create function to calculate one kind of percentage

--- components/percentage-component.tsx
+++ components/percentage-component.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
@@ -8,25 +10,40 @@ interface PercentageComponentProps {
 }
 
 export default function PercentageComponent({label1, label2}:PercentageComponentProps) {
+
+    const [result, setResult] = useState<number | null>()
+    const [op1, setOp1] = useState(0)
+    const [op2, setOp2] = useState(0)
+
+    const handleSubmit = () => {
+        if(op1 && op2){
+            setResult((op1 * op2) / 100 )
+        }
+    }
+
+    const clearResult = () => {
+        setResult(null)
+    }
+
     return (
         <Card className="w-full">
             <CardContent className="p-3">
                 <div className="flex flex-col p-3">
                     <div>
                         <label className="whitespace-nowrap text-lg">{label1}</label>
-                        <Input/>
+                        <Input type="number" onChange={(e) => setOp1(Number(e.target.value))}/>
                     </div>
                     <div>
                         <label className="text-lg">{label2}</label>
-                        <Input/>
+                        <Input type="number" onChange={(e) => setOp2(Number(e.target.value))}/>
                     </div>
                     <div className="flex-1">
-                        <label className="text-lg">Resultado</label>
-                        <div className="min-w-[70px] w-full h-10 border rounded-md">
-
+                        <label className="text-lg">Resultado:</label>
+                        <div className="min-w-[70px] w-full h-10 border rounded-md flex justify-center items-center text-lg">
+                            {result && (<h1>{result.toString()} %</h1>)}
                         </div>
-                        <Button className="w-full mt-4">Calcular</Button>
-                        <Button className="w-full mt-2" variant="outline">Limpar</Button>
+                        <Button onClick={handleSubmit} className="w-full mt-4">Calcular</Button>
+                        <Button onClick={clearResult} className="w-full mt-2" variant="outline">Limpar</Button>
                     </div>
                 </div>
             </CardContent>
