32e54b3df9af2ea29cdc4787f9c2aa463e1875d2
Victor Emanoel <victoremaanoel@outlook.com>
2024-11-18 18:32:04 -0300

    fix; commit before format pc

--- app/conversaoUnid/page.tsx
+++ app/conversaoUnid/page.tsx
@@ -1,54 +1,15 @@
 "use client"
 
-import ConversionTable from "@/components/conversion-table";
-import { Button } from "@/components/ui/button";
+import MassConversionComp from "@/components/mass-conversion-comp";
+import TemperatureConversionComp from "@/components/temperature-conversion";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Select, SelectGroup, SelectItem, SelectLabel, SelectContent, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { UnitToggle } from "@/components/unit-toggle";
 import { useState } from "react";
-
-type UnitType = "Tonelada" | "Quilograma" | "Hectograma" | "Decagrama" | "Grama" | "Decigrama" | "Centigrama" | "Miligrama";
-type ConversionResult = { unit: UnitType; value: number };
 
 export default function Conversao() {
 
     const [conversionType, setConversionType] = useState("");
-    const [amount, setAmount] = useState(0);
-    const [unit, setUnit] = useState<UnitType | undefined>(undefined);
-    const [conversionResults, setConversionResults] = useState<ConversionResult[]>([]);
 
-    const conversionRates = {
-        Tonelada: 1000000,
-        Quilograma: 1000,
-        Hectograma: 100,
-        Decagrama: 10,
-        Grama: 1,
-        Decigrama: 0.1,
-        Centigrama: 0.01,
-        Miligrama: 0.001,
-    };
-    
-    const convertUnits = (amount: number, fromUnit: UnitType) => {
-        const baseAmountInGrams = amount * conversionRates[fromUnit];
-        const results: ConversionResult[] = [];
-    
-        for (const [key, rate] of Object.entries(conversionRates)) {
-            results.push({ unit: key as UnitType, value: baseAmountInGrams / rate });
-        }
-    
-        return results;
-    };
-
-    const handleConvert = () => {
-        if (!unit) {
-            console.error("Selecione uma unidade de origem.");
-            return;
-        }
-        
-        const result = convertUnits(amount, unit);
-        setConversionResults(result); // Atualiza os resultados para exibição
-    };
 
     return (
         <main className="flex w-full justify-center">
@@ -66,48 +27,10 @@ export default function Conversao() {
                         <UnitToggle onSelected={setConversionType}/>
                     </div>
                     {conversionType && conversionType === "Massa" &&(
-                        <>
-                        <div className="flex gap-2 mt-5 mb-4 w-full">
-                            <Input placeholder="Quantidade" className="w-[40%]" onChange={(e) => {
-                                const value = e.target.value.replace(',', '.');
-                                const numericValue = Number(value);
-                                // Verifique se o valor é um número válido
-                                if (!isNaN(numericValue)) {
-                                    setAmount(numericValue);
-                                } else {
-                                    // Se não for um número válido, você pode definir amount como 0 ou deixar como está
-                                    setAmount(0);
-                                }
-                            }}/>
-                            <Select onValueChange={(value) => setUnit(value as UnitType)}>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Selcione uma unidade"/>
-                                </SelectTrigger>
-                                <SelectContent className="w-[60%]">
-                                    <SelectGroup>
-                                      <SelectLabel >Medidas de Massa</SelectLabel>
-                                      <SelectItem value="Tonelada">Tonelada (T)</SelectItem>
-                                      <SelectItem value="Quilograma">Quilograma (KG)</SelectItem>
-                                      <SelectItem value="Hectograma">Hectograma (HG)</SelectItem>
-                                      <SelectItem value="Decagrama">Decagrama (DAG)</SelectItem>
-                                      <SelectItem value="Grama">Grama (G)</SelectItem>
-                                      <SelectItem value="Decigrama">Decigrama (DG)</SelectItem>
-                                      <SelectItem value="Centigrama">Centigrama (CG)</SelectItem>
-                                      <SelectItem value="Miligrama">Miligrama (MG)</SelectItem>
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <Button onClick={handleConvert} className="w-full">Transformar</Button>
-                        {conversionResults.length > 0 && (
-                            <ConversionTable conversionResults={conversionResults} selectedUnit={unit}/>
-                        )}
-                        </>
+                        <MassConversionComp/>
                     )}
                     {conversionType && conversionType === "Temperatura" && (
-                        <div>
-                            T
-                        </div>
+                        <TemperatureConversionComp/>
                     )}
                     {conversionType && conversionType === "Velocidade" && (
                         <div>
---
+++ components/temperature-conversion.tsx
@@ -0,0 +1,97 @@
+import ConversionTable from "@/components/conversion-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectGroup, SelectItem, SelectLabel, SelectContent, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useState } from "react";
+
+type TemperatureType = "Celsius" | "Fahrenheit" | "Kelvin";
+type ConversionResultTemp = { unit: TemperatureType; value: number };
+
+function TemperatureConversionComp() {
+    const [amount, setAmount] = useState(0);
+    const [unit, setUnit] = useState<TemperatureType | undefined>(undefined);
+    const [conversionResults, setConversionResults] = useState<ConversionResultTemp[]>([]);
+
+    const conversionRatesTemperature = {
+        Celsius: 1,
+        Fahrenheit: 1.8,
+        Kelvin: 1, // Usamos a mesma base para Kelvin, mas precisamos ajustar a conversão
+    };
+    
+    const convertUnits = (amount: number, fromUnit: TemperatureType) => {
+        let baseAmountInCelsius;
+
+        // Converter a entrada para Celsius
+        if (fromUnit === "Celsius") {
+            baseAmountInCelsius = amount;
+        } else if (fromUnit === "Fahrenheit") {
+            baseAmountInCelsius = (amount - 32) / conversionRatesTemperature.Fahrenheit;
+        } else if (fromUnit === "Kelvin") {
+            baseAmountInCelsius = amount - 273.15;
+        } else {
+            throw new Error("Unidade de temperatura não reconhecida");
+        }
+
+        const results: ConversionResultTemp[] = [];
+        
+        // Calcular a conversão para todas as unidades
+        for (const [key, rate] of Object.entries(conversionRatesTemperature)) {
+            if (key === "Fahrenheit") {
+                results.push({ unit: key as TemperatureType, value: (baseAmountInCelsius * rate) + 32 });
+            } else if (key === "Kelvin") {
+                results.push({ unit: key as TemperatureType, value: baseAmountInCelsius + 273.15 });
+            } else {
+                results.push({ unit: key as TemperatureType, value: baseAmountInCelsius });
+            }
+        }
+
+        return results;
+    };
+
+    const handleConvert = () => {
+        if (!unit) {
+            console.error("Selecione uma unidade de origem.");
+            return;
+        }
+        
+        const result = convertUnits(amount, unit);
+        setConversionResults(result); // Atualiza os resultados para exibição
+    };
+
+    return (
+        <>
+        <div className="flex gap-2 mt-5 mb-4 w-full">
+            <Input placeholder="Valor" className="w-[40%]" onChange={(e) => {
+                const value = e.target.value.replace(',', '.');
+                const numericValue = Number(value);
+                // Verifique se o valor é um número válido
+                if (!isNaN(numericValue)) {
+                    setAmount(numericValue);
+                } else {
+                    // Se não for um número válido, você pode definir amount como 0 ou deixar como está
+                    setAmount(0);
+                }
+            }}/>
+            <Select onValueChange={(value) => setUnit(value as TemperatureType)}>
+                <SelectTrigger>
+                    <SelectValue placeholder="Selecione uma escala"/>
+                </SelectTrigger>
+                <SelectContent className="w-[60%]">
+                    <SelectGroup>
+                      <SelectLabel>Escalas de Temperatura</SelectLabel>
+                      <SelectItem value="Celsius">Celsius (°C)</SelectItem>
+                      <SelectItem value="Fahrenheit">Fahrenheit (°F)</SelectItem>
+                      <SelectItem value="Kelvin">Kelvin (K)</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+        </div>
+        <Button onClick={handleConvert} className="w-full">Transformar</Button>
+        {conversionResults.length > 0 && (
+            <ConversionTable conversionResults={conversionResults} selectedUnit={unit}/>
+        )}
+        </>
+    );
+}
+
+export default TemperatureConversionComp;
