e468bce16536ac3e2429e1643326bc26649d92ea
vctrr1 <victoremaanoel@outlook.com>
2025-08-05 02:15:39 -0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-
-
 export default function Home() {
   return (
-    <div className="flex flex-col w-full overflow-y-auto">
-      <h1>hello</h1>
-    </div>
+    <main className="flex flex-col items-center min-h-screen pt-10 pl-5 pr-5 md:justify-center">
+      <div className="rounded-lg shadow-2xl dark:shadow-[0_0_20px_rgba(255,255,255,0.2)] p-10 max-w-xl text-center">
+        <h1 className="text-7xl font-extrabold text-blue-500 mb-4">uTools</h1>
+        <p className="text-xl text-gray-700 dark:text-gray-300 mb-2">
+          O <span className="font-semibold text-blue-500">uTools</span> (Userful
+          Tools) é um site que reúne diversas ferramentas úteis para o seu dia a
+          dia, tudo em um só lugar.
+        </p>
+        <p className="text-md text-gray-600 dark:text-gray-400 text-lg">
+          Simplifique tarefas, aumente sua produtividade e encontre rapidamente
+          o que precisa, sem complicação.
+        </p>
+      </div>
+    </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,13 @@ import { ThemeSwitch } from "./theme-switch";
 import { SidebarTrigger } from "./ui/sidebar";
 
 const Navbar = () => {
-    return ( 
-        <div className="flex flex-row items-center w-full border-b justify-between p-3 pl-5 pr-5">
-            <SidebarTrigger/>
-            <h1 className="text-2xl">Utools</h1>
-            <ThemeSwitch/>
-        </div>
-    );
-}
- 
+  return (
+    <div className="flex flex-row items-center w-full border-b justify-between p-3 pl-5 pr-5">
+      <SidebarTrigger />
+      <h1 className="text-2xl">Utools</h1>
+      <ThemeSwitch />
+    </div>
+  );
+};
+
 export default Navbar;
